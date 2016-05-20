b760287f22fd40479c28c81c96d019ea470bc6bb
Jack Yeh <jack.yeh@YehJ.local>
2016-05-20 13:50:56 -0600

    display redux state inside of photo component: create individual photo js component; add hearts and message icon with corresponding number of hearts/likes and messages; add animation to likes and heart count

--- learn-redux/client/components/PhotoGrid.js
+++ learn-redux/client/components/PhotoGrid.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import Photo from './Photo';
 
 const PhotoGrid = React.createClass({
   render() {
     return (
       <div className="photo-grid">
-        I am the photogrid
+        {this.props.posts.map((post, i) => <Photo {...this.props} key={i} i={i} post={post} />)}
       </div>
     )
   }
-})
+});
 
 export default PhotoGrid;
