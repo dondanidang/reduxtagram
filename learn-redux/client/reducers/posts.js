8614290b4f38bc351faa92fe66bb24023dddadda
Jack Yeh <jack.yeh@YehJ.local>
2016-05-20 14:13:04 -0600

    add functionality: update state with reducers; updating likes when the like button is clicked; add onClick event listener

--- learn-redux/client/reducers/posts.js
+++ learn-redux/client/reducers/posts.js
@@ -3,6 +3,18 @@
 // 2. copy of current state
 
 function posts(state = [], action) {
+  switch(action.type) {
+    case 'INCREMENT_LIKES':
+      const i = action.index;
+      console.log('Incrementing Likes!!');
+      return [
+        ...state.slice(0,i), // up until before the one we are updating
+        {...state[i], likes: state[i].likes + 1},
+        ...state.slice(i+1), // up until after the one we are updating
+      ]
+    default:
+      return state;
+  }
   console.log(state, action);
   return state;
 }
